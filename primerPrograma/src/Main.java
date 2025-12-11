3ab4c8198794c8b2c7dbda8ddf03ba37a90ca96f
Fredy Lopez <fredygutierrez1661@gmail.com>
2025-12-10 19:58:44 -0600

    Agregando primer proyecto Java

---
+++ primerPrograma/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Bienvenido a Screen Match");
+        System.out.println("Pelicula: Matrix");
+
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        double media = (8.2 + 6.0 + 9.0) / 3;
+        System.out.println(media);
+    }
+}
