889fedece43530c8f713b45928afdca3fba6cb07
Fredy Lopez <fredygutierrez1661@gmail.com>
2025-12-11 19:53:13 -0600

    Creacion de nueva clase

---
+++ primerPrograma/src/Decisiones.java
@@ -0,0 +1,12 @@
+public class Decisiones {
+    public static void main(String[] args) {
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 9.2;
+
+
+        if (fechaDeLanzamiento > 2022){
+            System.out.println("Peliculas mas populares");
+        }
+    }
+}
