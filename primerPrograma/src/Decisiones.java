ac45a6694b1d96aa27deefa188c0ebc9ac881cd6
Fredy Lopez <fredygutierrez1661@gmail.com>
2025-12-11 20:49:04 -0600

    Creacion de nueva clase para el ingreso de datos

--- primerPrograma/src/Decisiones.java
+++ primerPrograma/src/Decisiones.java
@@ -1,12 +1,20 @@
 public class Decisiones {
     public static void main(String[] args) {
         int fechaDeLanzamiento = 1999;
-        boolean incluidoEnElPlan = true;
+        boolean incluidoEnElPlan = false;
         double notaDeLaPelicula = 9.2;
+        String tipoDeplan = "plus";
 
+//        if (fechaDeLanzamiento >= 2022){
+//            System.out.println("Peliculas mas populares");
+//        }else {
+//            System.out.println("Peliculas retro que aun vale la pena ver");
+//        }
 
-        if (fechaDeLanzamiento > 2022){
-            System.out.println("Peliculas mas populares");
+        if (incluidoEnElPlan && tipoDeplan.equals("plus")){
+            System.out.println("Disfrute de su pelicula");
+        }else {
+            System.out.println("Pelicula no disponible para su plan actual");
         }
     }
 }
